8bc92c06cb1f13a2578773e49607f7fb5836bfe4
Joyce Yan <joyceyan997@gmail.com>
2020-04-05 00:31:32 +1300

    Function Component -> Class Component Refer back to -> https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/14782806#overview 23 Dynamic Content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { render } from '@testing-library/react';
 
+class App extends Component{
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters:[
+        {
+          name: "Frankenstein",
+          id: "asr1"
+        },
+        {
+          name: "Dracula",
+          id: "asr2"
+        },
+        {
+          name: "Zombie",
+          id: "asr3"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className = "App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id} > {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
+/*
 function App() {
   return (
     <div className="App">
@@ -24,3 +59,4 @@ function App() {
 }
 
 export default App;
+*/
