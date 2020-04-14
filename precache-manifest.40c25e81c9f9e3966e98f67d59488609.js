7f5b9b4318a96d056837d13d4b178952d9210666
Joyce Yan <joyceyan997@gmail.com>
2020-04-15 11:14:47 +1200

    Updates

---
+++ precache-manifest.40c25e81c9f9e3966e98f67d59488609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018925e82e8cdddaa776c5519f94de0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16a1c54a382a524db1a8",
+    "url": "/monsters-rolodex/static/css/main.ba37e172.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a1c54a382a524db1a8",
+    "url": "/monsters-rolodex/static/js/main.917a7b07.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
